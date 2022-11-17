fdbff5f09f3664bce9c1133dab37d15e43e5f7e1
emmamelanight <e_melani@stu-digital-campus.fr>
2022-11-17 09:54:46 +0100

    ex5 modifié

--- ex5.js
+++ ex5.js
@@ -1,10 +1,19 @@
-function tri(tableau) {
-	console.log('tableau = ', tableau);
-	return tableau.sort((a,b) => a-b); // La fonction sort() permet de faire un tri
+// Tri par insertion
+function tri(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let tmp = arr[i];
+        let j = i - 1;
+        console.log('i = ',i, '; tmp = ',tmp, '; j =',j,' ; arr_j = ',arr[j]);
+        while (j >= 0 && arr[j] > tmp) {
+            arr[j + 1] = arr[j];
+            console.log('arr_'+(j+1)+' (1)', arr[j + 1]);
+            j--;
+        }
+        arr[j + 1] = tmp;
+        console.log('arr_' + (j + 1) +' (2)', arr[j + 1]);
+    }
+    return arr;
 }
 
 const monTableau = [7,3,1,8,12,2,5];
-console.log(tri(monTableau))
-
-
-// Il existe plusieurs algorithmes de tri : tri par insertion, tri par selection
+console.log(tri(monTableau));
